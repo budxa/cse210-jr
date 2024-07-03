6e410954b6e8473786e5edc6546417f94f27916c
budxa <jeraldmudzingwa@gmail.com>
2024-07-03 17:47:29 +0200

    Week 03 update...phew what a struggle

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture(new Reference("Proverbs", 91, 1, 2),
+         "He that dwelleth in the place of the most High shall abide under the shadow of the Almighty; I will say of the Lord, He is my refuge and my fortress: my God; in Him will I trust."
+        );
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press 'enter' to hide words or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+            else
+            {
+                scripture.HideRandomWords(3);
+            }
+        }
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine("All words are hidden.");
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    public Reference _reference;
+    private List<Word> _words;
+
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference ?? throw new ArgumentNullException(nameof(reference));
+        _words = text.Split(' ').Select(Word => new Word(Word)).ToList() ?? throw new ArgumentNullException(nameof(text));
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        numberToHide = 3;
+        Random _random = new Random();
+
+        var hWords = _random.Next(numberToHide);
+        var vWords = _words.Where(w => !w.IsHidden()).ToList();
+        for (int i = 0; i < hWords && vWords.Count > 0; i++)
+        {
+            var wordToHide = vWords[_random.Next(vWords.Count)];
+
+            wordToHide.Hide();
+            vWords.Remove(wordToHide);
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(word => word.IsHidden());
+    }
+
+    public string GetDisplayText()
+    {
+        string textReference = _reference.GetDisplayText();
+        string textScripture = string.Join(" ", _words.Select(word => word.GetDisplayText()));
+        return $"{ textReference} \n { textScripture}";
+    }
+}
