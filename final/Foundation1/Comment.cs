cedcb4cfe3bee5d0ef7c1d646c4bb24f557d1a6f
budxa <jeraldmudzingwa@gmail.com>
2024-07-26 08:06:03 +0200

    1st Foundation Program

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Comment
+{
+    public string _name;
+    public string _text;
+
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+}
--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+         var videos = new List<Video>
+        {
+            new Video("Video 1", "Author 1", 60)
+            {
+                _comments =
+                {
+                    new Comment("Commenter 1", "Comment 1 for video 1"),
+                    new Comment("Commenter 2", "Comment 2 for video 1"),
+                    new Comment("Commenter 3", "Comment 3 for video 1")
+                }
+            },
+            new Video("Video 2", "Author 2", 120)
+            {
+                _comments =
+                {
+                    new Comment("Commenter 1", "Comment 1 for video 2"),
+                    new Comment("Commenter 2", "Comment 2 for video 2"),
+                    new Comment("Commenter 3", "Comment 3 for video 2")
+                }
+            },
+            new Video("Video 3", "Author 3", 180)
+            {
+                _comments =
+                {
+                    new Comment("Commenter 1", "Comment 1 for video 3"),
+                    new Comment("Commenter 2", "Comment 2 for video 3"),
+                    new Comment("Commenter 3", "Comment 3 for video 3")
+                }
+            },
+            new Video("Video 4", "Author 4", 240)
+            {
+                _comments =
+                {
+                    new Comment("Commenter 1", "Comment 1 for video 4"),
+                    new Comment("Commenter 2", "Comment 2 for video 4"),
+                    new Comment("Commenter 3", "Comment 3 for video 4")
+                }
+            }
+        };
+
+        // Display video information and comments
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video._title}");
+            Console.WriteLine($"Author: {video._author}");
+            Console.WriteLine($"Length: {video._length} seconds");
+            Console.WriteLine($"Number of comments: {video.NumberOfComments}");
+
+            foreach (var comment in video._comments)
+            {
+                Console.WriteLine($"{comment._name}: {comment._text}");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+
     }
 }
